15ecd9eafd0a24a6a671ee8b633f917d3d79e6d6
franck <carnet.franck.paul@gmail.com>
2023-06-12 10:22:17 +0200

    Don't do things in frontend if plugin disabled

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'lazyLoading',
     'Implements lazy loading attribute for images and iframes',
     'Franck Paul',
-    '3.0.4',
+    '3.0.5',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
