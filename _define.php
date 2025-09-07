9eeb6e86a8dc920fbceab9687d26a76988bd5cf9
franck <carnet.franck.paul@gmail.com>
2025-09-07 15:59:26 +0200

    DC 2.36 compliance

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     '5.2',
     [
         'date'        => '2003-08-13T13:42:00+0100',
-        'requires'    => [['core', '2.28']],
+        'requires'    => [['core', '2.36']],
         'permissions' => 'My',
         'type'        => 'plugin',
         'settings'    => ['blog' => '#params.lazyloading_settings'],
@@ -26,5 +26,6 @@ $this->registerModule(
         'details'    => 'https://open-time.net/?q=lazyLoading',
         'support'    => 'https://github.com/franck-paul/lazyLoading',
         'repository' => 'https://raw.githubusercontent.com/franck-paul/lazyLoading/main/dcstore.xml',
+        'license'    => 'gpl2',
     ]
 );
