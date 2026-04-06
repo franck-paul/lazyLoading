c75aa6fb280ba69f5718d7cf7c2a131eb47096e2
franck <contact@open-time.net>
2026-04-06 14:44:02 +0200

    French locales

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -10,6 +10,9 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Implements lazy loading attribute for images and iframes'] = 'Implémente l\'attribut loading="lazy" pour les images et les iframes';
 L10n::$locales['lazyLoading'] = 'lazyLoading';
-L10n::$locales['Enable lazy loading implementation'] = 'Active la mise en place de l\'attribut loading="lazy" pour les images et les iframes';
+L10n::$locales['Implements lazy loading attribute for images and iframes'] = 'Implémente l\'attribut loading="lazy" pour les images et les iframes';
+L10n::$locales['Add the loading="lazy" attribute for images and iframes'] = 'Ajouter l\'attribut loading="lazy" pour les images et les iframes';
+L10n::$locales['Add the width and height attributes for images if none present'] = 'Ajouter les attributs de largeur et de hauteur pour les images s\'il n\'y en a pas.';
+L10n::$locales['Note that these two options are independent'] = 'Notez que ces deux options sont indépendantes';
+L10n::$locales['Unable to find the image file and extract its dimensions for '] = 'Impossible de trouver le fichier image et d\'en extraire les dimensions pour ';
