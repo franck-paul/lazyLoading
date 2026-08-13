7fbcef47a7ce02438e8c6a5070933323b94f45c7
franck <contact@open-time.net>
2026-08-13 16:42:20 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,21 +11,25 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'lazyLoading',
-    'Implements lazy loading attribute for images and iframes',
-    'Franck Paul',
-    '8.0',
-    [
-        'date'        => '2026-08-03T10:02:09+0200',
-        'requires'    => [['core', '2.39']],
-        'permissions' => 'My',
-        'type'        => 'plugin',
-        'settings'    => ['blog' => '#params.lazyloading_settings'],
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=lazyLoading',
-        'support'    => 'https://github.com/franck-paul/lazyLoading',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/lazyLoading/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'lazyLoading',
+        'Implements lazy loading attribute for images and iframes',
+        'Franck Paul',
+        '8.0',
+        [
+            'date'        => '2026-08-03T10:02:09+0200',
+            'requires'    => [['core', '2.39']],
+            'permissions' => 'My',
+            'type'        => 'plugin',
+            'settings'    => ['blog' => '#params.lazyloading_settings'],
+
+            'details'    => 'https://open-time.net/?q=lazyLoading',
+            'support'    => 'https://github.com/franck-paul/lazyLoading',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/lazyLoading/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
